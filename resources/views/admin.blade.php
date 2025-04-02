5a7f73ebe2f3ab15ddeda1d4e2e97ade86df2744
JuanCarlos0511 <jcguzman924324@gmail.com>
2025-04-02 10:59:01 -0600

    Separacion de estilos para repartir responsabilidades

--- resources/views/admin.blade.php
+++ resources/views/admin.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Clínica Intel - Panel de Administración</title>
-    <link rel="stylesheet" href="{{ asset('styles.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/main.css') }}">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
 </head>
 <body>
